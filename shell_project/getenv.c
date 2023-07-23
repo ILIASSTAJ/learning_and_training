6bdb7aead91c349b3a5bc1e016c610f1e42ab59d
ILIASSTAJ <iliassbasrtaj@gmail.com>
2023-07-23 13:01:20 +0200

    up dating

--- shell_project/getenv.c
+++ shell_project/getenv.c
@@ -16,7 +16,7 @@ char *_getenv(const char *name)
 		printf("%s\n", token);
 		if(strcmp(name, token) == 0)
 		{
-			return(token = strtok(NULL, "="));
+			return(token = strtok(NULL, "\n"));
 		}
 		i++;
 	}
@@ -24,16 +24,19 @@ char *_getenv(const char *name)
 }
 int main (void)
 {
-	/*ar *buffer;
+	char *buffer;
+	char *token;
 	size_t buffer_size;
 	int n_chars;
 	while(1)
 	{
 		write(1, "$ ", 2);
 		n_chars = getline(&buffer, &buffer_size, stdin);
-		printf("the value is equal to : %s\n",_getenv(buffer));
-	}*/
-	printf("%s\n", _getenv("PWD"));
+		token = strtok(buffer, " \t\n");
+		printf("the value of : %s is equal to : %s\n", token, _getenv(token));
+		
+	}
+	//printf("%s\n", _getenv("PWD"));
 	return 0;
 }
 
